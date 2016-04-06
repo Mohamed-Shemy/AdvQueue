c1ff47bb3950c3042e23946b3ac853fd5286d4bd
Mohamed Shemy <MOHAMED@mohameds-mac.local>
2016-04-06 06:40:36 +0200

    Initial Commit

---
+++ AdvQueue/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AdvQueue
+//
+//  Created by Mohamed Shemy on 4/6/16.
+//  Copyright © 2016 SHEMY. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
